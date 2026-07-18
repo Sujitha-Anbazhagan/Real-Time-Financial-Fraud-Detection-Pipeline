bbdee3db0ed148b549a83aa73950b3f2cd2c3e2f
Sujitha Anbazhagan <sujithamangai@gmail.com>
2026-07-18 16:41:05 +0530

    Implemented continuous PaySim transaction streaming producer

--- kafka/producer.py
+++ kafka/producer.py
@@ -2,51 +2,58 @@ from kafka import KafkaProducer
 import pandas as pd
 import json
 import time
+from datetime import datetime
 
 
-# Kafka configuration
 KAFKA_SERVER = "localhost:9092"
 TOPIC_NAME = "transactions"
 
+DATA_PATH = "data/PS_20174392719_1491204439457_log.csv"
 
-# Create Kafka Producer
+
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_SERVER,
     value_serializer=lambda x: json.dumps(x).encode("utf-8")
 )
 
 
-# Dataset path
-DATA_PATH = "data/PS_20174392719_1491204439457_log.csv"
+print("Loading PaySim dataset...")
 
 
-print("Loading dataset...")
+df = pd.read_csv(
+    DATA_PATH,
+    chunksize=1000
+)
 
-# Read dataset in chunks (important because file is 493 MB)
+
 count = 0
 
-for chunk in pd.read_csv(DATA_PATH, chunksize=1000):
+
+print("Starting transaction stream...")
+
+
+for chunk in df:
 
     for _, row in chunk.iterrows():
 
         transaction = row.to_dict()
+
+        transaction["timestamp"] = str(datetime.now())
+
 
         producer.send(
             TOPIC_NAME,
             value=transaction
         )
 
+
         count += 1
-        print("Sent transaction:", count)
 
-        if count == 1000:
-            break
 
-    if count == 1000:
-        break
+        print(
+            "Sent transaction:",
+            count
+        )
 
-producer.flush()
 
-print("All transactions sent successfully!")
-
-producer.close()
+        time.sleep(1)
